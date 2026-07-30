de645c399afefd0884e2f0fc6c842b746a774030
jay-java <jayprajapati.tops@gmail.com>
2026-07-30 15:06:35 +0530

    c++ intro

---
+++ C++ language/P001.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"hello c++";
+    int i;
+    for(i=1;i<=5;i++){
+        cout<<i<<endl;
+    }
+    cout<<"enter value";
+    int j;
+    cin>>j;
+    cout<<j;
+    return 0;
+}
